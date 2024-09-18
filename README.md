ea56356449a455ca5bf5105a5942da9e7320380f
Xavier Salazar <xavierssalazar92@gmail.com>
2024-09-18 13:22:02 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Owesome Collectibles
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl font-black">Owesome Collectibles</h1>
+    </>
+  );
+}
+
+export default App;
